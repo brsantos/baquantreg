71bfbc9be963240884bd74a0c63751d16ccee081
Bruno Santos <brunodosantos@gmail.com>
2015-12-08 18:15:05 -0200

    Changing to formula object in the arguments of the function.

--- R/zitobitQR.R
+++ R/zitobitQR.R
@@ -4,9 +4,11 @@
 #' at zero, where all zero observations are assumed to distributed according to a mixed
 #' discrete-continuous distribution.
 #'
+#' @param formula a formula object, with the response on the left of a ~
+#'  operator, and the terms, separated by + operators, on the right.
 #' @param tau Quantile of interest.
-#' @param y Vector of response variable.
-#' @param X Design matrix.
+#' @param data a data.frame from which to find the variables defined in the
+#' formula
 #' @param itNum Number of iterations.
 #' @param thin Thinning parameter.
 #' @param betaValue Initial values for the parameter beta for the continuous part.
@@ -16,32 +18,48 @@
 #' @param link Integer defining the link function used for the probability
 #' model. Default is 1.
 #' for the logit link function.
+#' @param priorVar Value that multiplies a identity matrix in the elicition
+#' process of the prior variance of the regression parameters.
+#' @param refresh Interval between printing a message during the iteration
+#' process. Default is set to 100.
 #' @return A list with the chains of all parameters of interest.
 #' @references Santos and Bolfarine (2015) - Bayesian quantile regression analysis for continuous data
 #' with a discrete component at zero. \emph{Preprint}. \url{http://arxiv.org/abs/1511.05925}
 #' @export
 #' @useDynLib baquantreg
+#' @import RcppArmadillo
+#' @import RcppGSL
+#' @importFrom Rcpp evalCpp
 #' @examples
 #' set.seed(1)
+#' data("BrazilDurableGoods")
+#' # Change the number of iterations for better results.
+#' model <- zitobitQR(expenditure ~ age + education, tau=0.5,
+#'                   data=BrazilDurableGoods, itNum=100,
+#'                   sigmaGamma=0.10, refresh=20)
 
-zitobitQR <- function(tau = 0.5, y, X, itNum, thin=1,
-                      betaValue=rep(0, dim(X)[2]),
-                      sigmaValue=1, gammaValue=rep(0, dim(X)[2]),
-                      sigmaGamma = 0.5, link=1){
+zitobitQR <- function(formula, tau = 0.5, data, itNum, thin=1,
+                      betaValue = NULL, sigmaValue=1,
+                      gammaValue = NULL,  sigmaGamma = 0.5,
+                      link=1, priorVar = 100, refresh = 100){
+
+  y <- as.numeric(model.extract(model.frame(formula, data), 'response'))
+  X <- model.matrix(formula, data)
+
+  if (is.null(betaValue)) betaValue <- rep(0, dim(X)[2])
+  if (is.null(gammaValue)) gammaValue <- rep(0, dim(X)[2])
 
   ziTobit <- ziTobitBayesQR(tau = tau, y=y, X=X, itNum=itNum, thin=thin,
                           betaValue=betaValue, sigmaValue=sigmaValue,
-                          gammaValue = gammaValue,
-                          sigmaGamma=sigmaGamma, link=link)
+                          gammaValue=gammaValue, sigmaGamma=sigmaGamma,
+                          link=link, priorVar=priorVar, refresh=refresh)
 
   ziTobit$tau <- tau
-  ziTobit$y <- y
-  ziTobit$X <- X
+  ziTobit$formula <- formula
+  ziTobit$data <- data
   ziTobit$link <- link
 
   class(ziTobit) <- "zitobitQR"
 
   return(ziTobit)
 }
-
-
