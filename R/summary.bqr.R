8fa5b7f3dc796cd720f89aa57aebc1a71412f471
Bruno Santos <brunodosantos@gmail.com>
2016-01-08 18:22:35 -0200

    Adding functions summary.bqr and plot.summary.bqr

---
+++ R/summary.bqr.R
@@ -0,0 +1,60 @@
+#' Summary methods for Bayesian quantile regression models.
+#'
+#' Returns a summary data.frame for a Bayesian quantile regression fit for more
+#'  than one quantile.
+#'
+#' @param object This is an object of class "bqrs", produced by a call to the
+#'  bayesQR function.
+#' @param burnin Initial part of the chain, which is to be discarded. Default
+#'  value is 1000.
+#' @return A data frame with summary information about the quantile regression
+#'  parameters.
+#' @export
+#' @useDynLib baquantreg
+#' @examples
+#' set.seed(1)
+
+
+summary.bqr <- function (object, burnin = 1000, ci = 0.95, ...)
+{
+  if (class(object) != "bqr")
+    stop("Use the correct summary method for your model")
+
+  numIt <- dim(object$chains[[1]]$BetaSample)[1]
+
+  X <- model.matrix(object$formula, object$data)
+
+  output <- list()
+  output$BetaPosterior <- lapply(object$chains, function(a){
+    vnames <- colnames(X)
+
+    coef <- apply(a$BetaSample[(burnin+1):numIt, ], 2, mean)
+    quantilesL <- apply(a$BetaSample[(burnin+1):numIt, ], 2, quantile, (1-ci)/2)
+    quantilesU <- apply(a$BetaSample[(burnin+1):numIt, ], 2, quantile, 1-(1-ci)/2)
+
+    data.frame(variable = vnames,
+               coef = coef,
+               lower = quantilesL,
+               upper = quantilesU)
+  })
+
+  names(output$BetaPosterior) <- paste("Tau = ", object$tau)
+
+  output$SigmaPosterior <- data.frame(t(sapply(object$chains, function(a){
+    meanSigma <- mean(a$SigmaSample[(burnin+1):numIt])
+    quantilesL <- quantile(a$SigmaSample[(burnin+1):numIt], (1-ci)/2)
+    quantilesU <- quantile(a$SigmaSample[(burnin+1):numIt], 1-(1-ci)/2)
+
+    c(meanSigma, quantilesL, quantilesU)
+  })))
+
+  colnames(output$SigmaPosterior) <- c("Mean", "Lower", "Upper")
+
+  tau <- object$tau
+  output$SigmaPosterior <- data.frame(cbind(tau, output$SigmaPosterior))
+
+  output$taus <- object$tau
+
+  class(output) <- "summary.bqr"
+  output
+}
