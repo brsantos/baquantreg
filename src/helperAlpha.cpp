5dfe06d821c3203a433a2398884bbe18e4c73da0
Bruno Santos <brunodosantos@gmail.com>
2016-03-04 11:42:08 -0300

    Using Cholesky decomposition to invert matrices.

--- src/helperAlpha.cpp
+++ src/helperAlpha.cpp
@@ -16,12 +16,12 @@ double logLikelihoodAlpha (double alpha, arma::mat aux, arma::mat diagU,
   arma::mat covMatAux(n,n, arma::fill::zeros);
   covMatAux.diag().fill(alpha + jitter);
 
-  arma::mat cholCov = chol((1-alpha)*covMat + covMatAux).i();
-  arma::mat covMatInv = cholCov * cholCov.t();
+  arma::mat cholCov = chol((1-alpha)*covMat + covMatAux, "lower");
+  arma::mat covMatInv = solve(trimatl(cholCov),diagU*aux);
 
-  arma::log_det(valDet, sign, (1-alpha)*covMat);
+  arma::log_det(valDet, sign, (1-alpha)*covMat+covMatAux);
 
-  output = -0.5*valDet-0.5*(as_scalar(aux.t()*diagU*covMatInv*diagU*aux));
+  output = -0.5*valDet-0.5*(as_scalar(covMatInv.t()*covMatInv));
 
   return output;
 }
@@ -37,27 +37,25 @@ double logLikelihoodAlpha2 (double alpha, arma::mat aux, arma::mat diagU,
   double sign;
 
   arma::mat auxCov(m, m, arma::fill::zeros);
-
   auxCov.diag().fill(alpha + jitter);
 
-  arma::mat cholCov = arma::chol((1-alpha)*covMat2 + auxCov).i();
-  arma::mat covMatInv =  cholCov * cholCov.t();
-  arma::mat matAux = covMatAux*covMatInv*covMatAux.t();
-  arma::mat sigmaDot = diagmat(alpha +
-    (1-alpha)*(covMat.diag() - matAux.diag())).i();
+  arma::mat cholCov = arma::chol((1-alpha)*covMat2 + auxCov, "lower");
+  arma::mat covMatInv =  solve(trimatl(cholCov),covMatAux.t());
+  arma::mat matAux = covMatInv.t()*covMatInv;
+  arma::mat sigmaDot = diagmat(1/(alpha +
+    (1-alpha)*(covMat.diag() - matAux.diag())));
 
-  arma::mat cholCov2 = (1-alpha)*covMat2 +
-    covMatAux.t()*sigmaDot*covMatAux;
+  arma::mat cholCov2 = (1-alpha)*covMat2 + covMatAux.t()*sigmaDot*covMatAux;
 
-  arma::mat matM = arma::chol(cholCov2 + auxCov).i();
-  arma::mat matM2 = matM * matM.t();
+  arma::mat matM = arma::chol(cholCov2 + auxCov, "lower");
+  arma::mat matM2 = solve(trimatl(matM), covMatAux.t());
+  arma::mat matM3 = matM2.t() * matM2;
 
-  arma::mat CovCov = sigmaDot -
-    sigmaDot * covMatAux * matM2 * covMatAux.t() * sigmaDot;
+  arma::mat CovCov = sigmaDot - sigmaDot * matM3 * sigmaDot;
 
-  arma::log_det(valDet, sign, matAux + sigmaDot.i());
+  arma::log_det(valDet, sign, matAux + diagmat(1/sigmaDot.diag()));
 
-  output = -0.5*valDet-0.5*(as_scalar(aux.t()*diagU*covMatInv*diagU*aux));
+  output = -0.5*valDet-0.5*(as_scalar(aux.t()*diagU*CovCov*diagU*aux));
 
   return output;
 }
@@ -73,7 +71,7 @@ double mhAlpha(double alpha, arma::mat aux, arma::mat diagU,
   p1 = alpha * precision;
   p2 = (1-alpha) * precision;
   // alphaProp = rbeta(1, p1, p2)[0];
-  alphaProp = runif(1)[0]*0.8 + 0.1;
+  alphaProp = runif(1)[0];
 
   postCur = logLikelihoodAlpha(alpha, aux, diagU, covMat, jitter);
   postProp = logLikelihoodAlpha(alphaProp, aux, diagU, covMat, jitter);
@@ -99,16 +97,16 @@ double mhAlpha2(double alpha, arma::mat aux, arma::mat diagU,
   p1 = alpha * precision;
   p2 = (1-alpha) * precision;
   // alphaProp = rbeta(1, p1, p2)[0];
-  alphaProp = runif(1)[0]*0.8 + 0.1;
+  alphaProp = runif(1)[0];
 
   postCur = logLikelihoodAlpha2(alpha, aux, diagU, covMat, covMat2, covMatAux,
                                 jitter, indices, m);
   postProp = logLikelihoodAlpha2(alphaProp, aux, diagU, covMat, covMat2,
                                  covMatAux, jitter, indices, m);
 
-  double logAccepProb = postProp/postCur;
+  double logAccepProb = postProp - postCur;
 
-  if(runif(1)[0] < logAccepProb) new_alpha = alphaProp;
+  if(log(runif(1)[0]) < logAccepProb) new_alpha = alphaProp;
   else new_alpha = alpha;
 
   return new_alpha;
