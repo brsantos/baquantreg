fae76e0a02824bcd328451f1f91500830f0cf021
brsantos <brunodosantos@gmail.com>
2016-03-08 16:58:42 -0300

    Changing proposal density for the latent variables in the multiple-try Metropolis algorithm.

--- src/helperGIG2.cpp
+++ src/helperGIG2.cpp
@@ -27,39 +27,45 @@ double logPosteriorV2(arma::vec aux, double theta, double psi2, double sigma,
     (1/sigma)*sum(vSample);
 }
 
+
+NumericVector calcLogWeights(NumericVector weights){
+  double a0 = max(weights);
+  NumericVector output = exp(weights - a0)/sum(exp(weights - a0));
+  return output;
+}
+
 double mtM(arma::vec aux, double theta, double psi2, double sigma,
                arma::vec vSample, double curV, int indice, arma::mat C,
                double tuneV, int k){
 
   double output;
   int nNeg, nNegRef;
-  NumericVector prop(k), xRef(k), weights(k), weightsRef(k);
+  NumericVector prop(k), xRef(k), weights(k), weights2(k), weightsRef(k);
+  prop = rexp(k, tuneV);
   for (int i = 0; i < k; i++){
-    prop[i] = rnorm(1, curV, tuneV)[0];
-    while(prop[i] < 0) prop[i] = rnorm(1, curV, tuneV)[0];
-    weights[i] = exp(logPosteriorV(aux, theta, psi2, sigma, vSample, prop[i],
-                                   C, indice));
+    weights[i] = logPosteriorV(aux, theta, psi2, sigma, vSample, prop[i],
+                                   C, indice);
   }
-  double y = RcppArmadillo::sample(prop, 1, false, weights)[0];
+
+  weights += log(dexp(prop, tuneV));
+  weights2 = calcLogWeights(weights);
+
+  double y = RcppArmadillo::sample(prop, 1, false, weights2)[0];
   for (int ii = 0; ii < k-1; ii++){
-    xRef[ii] = rnorm(1, y, tuneV)[0];
-    while(xRef[ii] < 0) xRef[ii] = rnorm(1, y, tuneV)[0];
-    weightsRef[ii] = exp(logPosteriorV(aux, theta, psi2, sigma, vSample, xRef[ii],
-                                       C, indice));
+    xRef[ii] = rexp(1, tuneV)[0];
+    weightsRef[ii] = logPosteriorV(aux, theta, psi2, sigma, vSample, xRef[ii],
+                                       C, indice);
   }
   xRef[k-1] = curV;
-  weightsRef[k-1] = exp(logPosteriorV(aux, theta, psi2, sigma, vSample, xRef[k-1],
-                                      C, indice));
+  weightsRef[k-1] = logPosteriorV(aux, theta, psi2, sigma, vSample, xRef[k-1],
+                                      C, indice);
 
-  double probA = sum(weights)/sum(weightsRef);
-  if (runif(1)[0] < probA) output = y;
+  weightsRef += log(dexp(xRef, tuneV));
+
+  double probA = (max(weights) + log(sum(exp(weights - max(weights))))) -
+    (max(weightsRef) + sum(exp(weightsRef - max(weightsRef))));
+  if (runif(1)[0] < exp(probA)) output = y;
   else output = curV;
-  return output;
-}
-
-NumericVector calcLogWeights(NumericVector weights){
-  double a0 = max(weights);
-  NumericVector output = exp(weights - a0)/sum(exp(weights - a0));
   return output;
 }
 
@@ -82,7 +88,7 @@ NumericVector mtM2(arma::vec aux, double theta, double psi2, double sigma,
   int y = RcppArmadillo::sample(indices, 1, false, weights2)[0];
   for (int ii = 0; ii < k-1; ii++){
     xRef(_,ii) = rexp(n, tuneV);
-    weights[ii] = logPosteriorV2(aux, theta, psi2, sigma, xRef(_,ii), C) +
+    weightsRef[ii] = logPosteriorV2(aux, theta, psi2, sigma, xRef(_,ii), C) +
       sum(log(dexp(xRef(_,ii), tuneV)));
   }
   xRef(_,k-1) = NumericVector(vSample.begin(), vSample.end());
@@ -91,8 +97,10 @@ NumericVector mtM2(arma::vec aux, double theta, double psi2, double sigma,
 
   NumericVector weightsRef2 = calcLogWeights(weightsRef);
 
-  double probA = sum(weights2)/sum(weightsRef2);
-  if (runif(1)[0] < probA) output = prop( _, y-1);
+  double probA = (max(weights) + log(sum(exp(weights - max(weights))))) -
+    (max(weightsRef) + sum(exp(weightsRef - max(weightsRef))));
+
+  if (runif(1)[0] < exp(probA)) output = prop( _, y-1);
   else output = NumericVector(vSample.begin(), vSample.end());
   return output;
 }
