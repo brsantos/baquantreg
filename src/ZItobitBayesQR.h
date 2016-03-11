23dffe7d204c161ea168d17f9bf13193ca9a39aa
brsantos <brunodosantos@gmail.com>
2016-03-11 15:07:43 -0300

    Deleting unncessary header file.

--- src/ZItobitBayesQR.h
+++
@@ -1,3 +0,0 @@
-List ziTobitBayesQR(double tau, arma::colvec y, arma::mat X, int itNum, int thin,
-                      arma::colvec betaValue, double sigmaValue, arma::colvec betaZeroValue,
-                      double sigmaBetaZero, int link);
