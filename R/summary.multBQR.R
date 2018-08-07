dd0c5e1d35f7838e9a95eb46f55de9b37336e3e8
Bruno Santos <brunodosantos@gmail.com>
2018-08-07 09:32:18 +0200

    Changing Makevars.win

--- R/summary.multBQR.R
+++ R/summary.multBQR.R
@@ -4,6 +4,8 @@ summary.multBQR <- function(object, ...){
   if (object$method == 'rcpp')
     lapply(object, summary.bqr, mult = TRUE, ...)
   else {
-
+    lapply(object, function(a){
+      a$models
+    })
   }
 }
